155eec7e1e083ade681ac5fd4e82ca05c2072f3f
Thomas Beduneau <thomas@switch-company.com>
2019-10-22 19:12:38 +0200

    use incomming webhook to post the response

--- src/functions/skill.js
+++ src/functions/skill.js
@@ -1,23 +1,55 @@
 const {URLSearchParams} = require( 'url' );
 const {mappings} = require( '../mappings' );
 const {skills} = require( '../skills' );
+const https = require('https');
+const url = require('url');
 
-exports.handler = function(event, context, callback) {
-  const textParser = /^(\w*)\s?(\((\w*),\s(\w*)\)|check)?\s?(.*)?$/;
-  const baseURL = process.env.URL;
+const slackEndpoint = url.parse(process.env.SLACK_HOOK);
+
+const send = (data) => {
+  const options = {
+    hostname: slackEndpoint.hostname,
+    port: 443,
+    path: slackEndpoint.path,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, function (res) {
+      let chunks = [];
+
+      res.on("data", (chunk) => {
+        chunks.push(chunk);
+      });
+
+      res.on("error", (error)  =>{
+        reject( error );
+      });
+
+      res.on("end", ()  =>{
+        var body = Buffer.concat(chunks);
+        resolve(body.toString());
+      });
+    });
+
+    req.write(data);
+    req.end();
+  });
+}
+
+exports.handler = async function(event, context, callback) {
+  const textParser = /^(?<skill>\w*)\s?(?<check>\((?<level>\w*),\s(?<status>\w*)\)|check)?\s?(?<text>.*)?$/;
+  const baseURL = 'https://disco-bot.netlify.com'; //process.env.URL;
 
   const params = new URLSearchParams(event.body);
-  const query = params.get('text')
+  const query = params.get('text');
+  const channelId = params.get('channel_id');
 
-  const parsed = textParser.exec(query);
-
-  const groups = {
-    skill: parsed[1],
-    check: parsed[2],
-    level: parsed[3],
-    status: parsed[4],
-    text: parsed[5]
-  }
+  const {groups} = textParser.exec(query);
 
   const skill = mappings.shortcuts[ groups.skill ] || groups.skill;
 
@@ -55,21 +87,24 @@ exports.handler = function(event, context, callback) {
 
   const icon = `${baseURL}/assets/skills/${skill}.png`;
 
-  callback( null, {
+  const response = await send(JSON.stringify({
+    channel: channelId,
+    username: username.join(' '),
+    icon_url: icon,
+    attachments: [
+      {
+        color,
+        text,
+      }
+    ]
+  }))
+    .catch(error => {
+      console.error( error );
+      return 'fail'
+    });
+
+  return {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8'
-    },
-    body: JSON.stringify({
-      response_type: 'in_channel',
-      username: username.join(' '),
-      icon_url: icon,
-      attachments: [
-        {
-          color,
-          text,
-        }
-      ]
-    })
-  });
+    body: response
+  };
 }
--- src/skills/encyclopedia.js
+++ src/skills/encyclopedia.js
@@ -1,6 +1,6 @@
 exports.encyclopedia = {
   name: 'Encyclopedia',
   texts: [
-    'That isn’t *just* a five-pointed star -- it’s an inverted white pentagram cradled in a wreath of antlers. The iconography of communism, in other words.'
+    'That isn\'t *just* a five-pointed star -- it\'s an inverted white pentagram cradled in a wreath of antlers. The iconography of communism, in other words.'
   ]
 }
